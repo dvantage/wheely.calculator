b8297ccd4bc0858ae0bb0e4d11d0015120b6fa20
Vladimir Dorogov <i@d-vantage.com>
2019-06-02 18:34:46 +0300

    initial commit

---
+++ precache-manifest.c2266628ef51011d47da598dc2c43f20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "15842ebd22c282e98d6b",
+    "url": "/js/chunk-vendors.72cdc336.js"
+  },
+  {
+    "revision": "896d16d63d5793615332",
+    "url": "/js/app.b96e60fc.js"
+  },
+  {
+    "revision": "5ade2637f8496449f906dabcd21797e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a92561d82162667beb616e5d46a0f5a",
+    "url": "/img/background.1a92561d.png"
+  },
+  {
+    "revision": "896d16d63d5793615332",
+    "url": "/css/app.19e92e79.css"
+  }
+];
